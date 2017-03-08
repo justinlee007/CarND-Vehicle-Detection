2aec6c48ac7441442690f1bc6314f33090e7120d
Justin Lee <justin.lee007@gmail.com>
2017-03-08 09:07:59 -0800

    added front-matter pick for hog

--- get_hog.py
+++ get_hog.py
@@ -1,41 +1,44 @@
-import glob
+import argparse
+import os
 
+import cv2
 import matplotlib
 
 matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 
-from detection import *
+import detection
 
 if __name__ == "__main__":
-    not_car_images = glob.glob("non-vehicles/*/*.png")
-    car_images = glob.glob("vehicles/*/*.png")
-    cars = []
-    notcars = []
+    parser = argparse.ArgumentParser(description="Utility for visualizing HOG feature extraction")
+    parser.add_argument("-show", action="store_true", help="Visualize HOG image")
+    parser.add_argument("-save", action="store_true", help="Save HOG image")
+    results = parser.parse_args()
+    visualize = bool(results.show)
+    save_examples = bool(results.save)
 
-    print("not_car_images size={}, car_images size={}".format(len(not_car_images), len(car_images)))
-    for image in not_car_images:
-        notcars.append(image)
-    for image in car_images:
-        cars.append(image)
-    # Generate a random index to look at a car image
-    ind = np.random.randint(0, len(cars))
     # Read in the image
-    image = mpimg.imread(cars[ind])
+    image_file = "./test_images/31.png"
+    image = mpimg.imread(image_file)
     gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
     # Define HOG parameters
     orient = 9
     pix_per_cell = 8
     cell_per_block = 2
     # Call our function with vis=True to see an image output
-    features, hog_image = get_hog_features(gray, orient, pix_per_cell, cell_per_block, vis=True, feature_vec=False)
+    features, hog_image = detection.get_hog_features(gray, orient, pix_per_cell, cell_per_block, vis=True,
+                                                     feature_vec=False)
 
     # Plot the examples
-    fig = plt.figure()
-    plt.subplot(121)
-    plt.imshow(image, cmap="gray")
-    plt.title("Example Car Image")
-    plt.subplot(122)
-    plt.imshow(hog_image, cmap="gray")
-    plt.title("HOG Visualization")
-    plt.show()
+    f, (ax1, ax2) = plt.subplots(1, 2, figsize=(14, 7))
+    ax1.imshow(image, cmap="gray")
+    ax1.set_title("Example Car Image", fontsize=24)
+    ax2.imshow(hog_image, cmap="gray")
+    ax2.set_title("HOG Visualization", fontsize=24)
+    if visualize:
+        plt.show()
+    if save_examples:
+        save_file_name = "hog_features_{}".format(os.path.basename(image_file.replace(".jpg", ".png")))
+        save_location = "./output_images/{}".format(save_file_name)
+        f.savefig(save_location, bbox_inches="tight")
